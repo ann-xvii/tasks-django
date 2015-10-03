cb88c16f3957b87e163f67b323fa34f9bb363900
Anna Atiba <aatiba@crowdrise.com>
2015-10-02 19:08:30 -0700

    update settings, add static files and templates, tests

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from .secret import *
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DJANGO_SETTINGS_MODULE="taskbuster.settings.development"
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'taskbuster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,3 +100,7 @@ TEST_RUNNER = 'rainbowtests.test.runner.RainbowDiscoverRunner'
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
